bf1d90b2c1c98740d5ad63dc6b60c1913904f348
Linamohamed89 <linaaloqaily1989@gmail.com>
2023-08-19 14:25:37 +0200

    create a new section -login form container-

--- js/script.js
+++ js/script.js
@@ -4,5 +4,30 @@ let navbar=document.querySelector('.navbar');
 
 
 menu.onclick = () =>{
-    menu.classList.toggle('fa-times')
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('.active');
+    }else{
+        document.querySelector('.header').classList.remove('.active');
+    }
+    menu.classList.remove('fa-times');
+    navbar.classList.toggle('active');
+}
+
+
+
+window.onload = () =>{
+
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('.active');
+    }else{
+        document.querySelector('.header').classList.remove('.active');
+    }
 }
